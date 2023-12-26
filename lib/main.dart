9b041a97ea7d44b733047daf195884d204d0dee1
Ahmed khaled <ahmedkh8907@gmail.com>
2023-12-26 14:22:16 +0200

    provide addNoteCubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes_app/constants.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/notes_view.dart';
 
@@ -9,7 +11,7 @@ void main() async {
   await Hive.initFlutter();
   await Hive.openBox(kNotesBox);
   Hive.registerAdapter(NoteModelAdapter());
-  
+
   runApp(const NotesApp());
 }
 
@@ -23,11 +25,19 @@ class NotesApp extends StatelessWidget {
       minTextAdapt: true,
       splitScreenMode: true,
       child: Builder(builder: (context) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: 'Notes App',
-          theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
-          home: const NotesView(),
+        return MultiBlocProvider(
+          providers: [
+            BlocProvider(
+              create: (context) => AddNoteCubit(),
+            ),
+          ],
+          child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Notes App',
+            theme:
+                ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
+            home: const NotesView(),
+          ),
         );
       }),
     );
