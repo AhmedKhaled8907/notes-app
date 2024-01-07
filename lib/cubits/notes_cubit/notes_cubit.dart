209f2cd6c4ac9ff9debf74d000245b1ace7948a6
Ahmed khaled <ahmedkh8907@gmail.com>
2024-01-07 17:33:44 +0200

    refactor the code

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -1,7 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/foundation.dart';
 import 'package:hive/hive.dart';
-import 'package:notes_app/constants.dart';
+import 'package:notes_app/constants/constants.dart';
 import 'package:notes_app/models/note_model.dart';
 
 part 'notes_state.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,9 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
-import 'package:notes_app/constants.dart';
+import 'package:notes_app/constants/constants.dart';
 import 'package:notes_app/models/note_model.dart';
-import 'package:notes_app/views/app_bloc_observer.dart';
+import 'package:notes_app/cubits/app_bloc_observer.dart';
 import 'package:notes_app/views/notes_view.dart';
 
 void main() async {
