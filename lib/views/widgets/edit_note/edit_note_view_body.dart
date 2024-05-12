593fe538c9b8bc049892a989309e5c6fef409412
Ahmed khaled <ahmedkh8907@gmail.com>
2024-05-12 18:47:12 +0300

    updating some packages

--- lib/views/widgets/edit_note/edit_note_view_body.dart
+++ lib/views/widgets/edit_note/edit_note_view_body.dart
@@ -4,7 +4,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/widgets/custom_widgets/custom_edit_text_form_field.dart';
-import 'package:notes_app/views/widgets/custom_widgets/custom_text_form_field.dart';
 import 'package:notes_app/views/widgets/edit_note/edit_note_color_list.dart';
 import 'package:notes_app/views/widgets/notes/custom_app_bar.dart';
 
